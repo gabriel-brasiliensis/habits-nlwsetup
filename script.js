a28562e50ac3664ec3688b8eee49e127f96ce20c
Gabriel Gusmão <gabriel.brasiliensis@gmail.com>
2023-01-26 10:25:35 -0300

    initial modal interaction

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+const modal = document.getElementById("modal");
 const form = document.querySelector("#habits-form");
-const button = document.querySelector("header button");
+const registerDayButton = document.querySelector("#register-day");
+const modalShowButton = document.querySelector("#show-modal")
+const modalCloseButton = document.querySelector("#close-modal")
 
 const nlwSetup = new NLWSetup(form);
 
-button.addEventListener("click", add);
+registerDayButton.addEventListener("click", addDay);
+modalShowButton.addEventListener("click", showModal)
+modalCloseButton.addEventListener("click", hideModal)
 form.addEventListener("change", save);
 
-function add() {
+function showModal() {
+  modal.classList.add("show-modal")
+}
+
+function hideModal() {
+  modal.classList.remove("show-modal")
+}
+
+function addDay() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
 
   const dayExists = nlwSetup.dayExists(today);
@@ -18,6 +31,10 @@ function add() {
 
   alert("Esse dia ainda não existe, estamos criando ele agora.");
   nlwSetup.addDay(today);
+}
+
+function addHabit() {
+
 }
 
 function save() {
